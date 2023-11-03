2a9ade6160ceb809013508e0de57505681d99e2f
Laurent <laurent2002ruihong@yahou.com>
2023-11-03 14:47:08 +0100

    derniere version

--- app/src/main/java/com/example/projet_android/CreateAccountActivity.java
+++ app/src/main/java/com/example/projet_android/CreateAccountActivity.java
@@ -3,6 +3,8 @@ package com.example.projet_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_account);
+
+        final Button aBtnLogin = findViewById(R.id.button4);
+        aBtnLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.activity_main);
+
+            }
+        });
+        final Button aBtnCreate = findViewById(R.id.button3);
+        aBtnCreate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            }
+        });
+
+    }
     }
 }
