0fc62767a45677ce6c02c6bbf2ed62826f8582e1
Laurent <laurent2002ruihong@yahou.com>
2023-11-03 13:38:05 +0100

    3/11

--- app/src/main/java/com/example/projet_android/MainActivity.java
+++ app/src/main/java/com/example/projet_android/MainActivity.java
@@ -3,10 +3,15 @@ package com.example.projet_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+
 
 //test modif
 
 public class MainActivity extends AppCompatActivity {
+
+    Button pBtnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
