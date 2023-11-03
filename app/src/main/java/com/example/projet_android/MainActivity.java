616de4c92aa8849bc7376b1e719ddb881a1d72e5
Valentin Vatamanescu <valentin14062002@gmail.com>
2023-11-03 15:59:09 +0100

    Changement logo appli et interface welcome

--- app/src/main/java/com/example/projet_android/MainActivity.java
+++ app/src/main/java/com/example/projet_android/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +14,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        ImageView myImageView = (ImageView) findViewById(R.id.imageView);
+        myImageView.setImageResource(R.drawable.logo);
+
         final Button aBtnLogin = findViewById(R.id.button2);
         aBtnLogin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
