673ffeec146b48c5a4b8287d29abdb3f23354a24
Laurent <laurent2002ruihong@yahou.com>
2023-11-03 14:00:43 +0100

    Bouton Login qui renvoie sur l'interface principal sans le login/mdp

--- app/src/main/java/com/example/projet_android/MainActivity.java
+++ app/src/main/java/com/example/projet_android/MainActivity.java
@@ -3,22 +3,22 @@ package com.example.projet_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 
-
-//test modif
-
 public class MainActivity extends AppCompatActivity {
-
-    Button pBtnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        final Button aBtnLogin = findViewById(R.id.button2);
+        aBtnLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.interface_principale);
+            }
+        });
     }
-
-
 }
