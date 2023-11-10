2716ed90813a7ba566e249afe8723138946fc063
Laurent <laurent2002ruihong@yahou.com>
2023-11-10 19:33:58 +0100

    rattachement inferface de conversation

--- app/src/main/java/com/example/projet_android/InterfacePrincipalActivity.java
+++ app/src/main/java/com/example/projet_android/InterfacePrincipalActivity.java
@@ -3,6 +3,7 @@ package com.example.projet_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -27,6 +28,14 @@ public class InterfacePrincipalActivity extends AppCompatActivity {
         pBtnProfil.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent it = new Intent(getApplicationContext(), PageMonProfilActivity.class);
+                startActivity(it);
+            }
+        });
+
+        final ImageButton pBtnConv= (ImageButton) findViewById(R.id.imageButtonConversation);
+        pBtnConv.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent it= new Intent(getApplicationContext(), InterfaceConversationActivity.class );
                 startActivity(it);
             }
         });
