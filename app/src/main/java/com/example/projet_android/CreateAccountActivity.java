259e4420fbfde802c00e15d82bc1928f7154b0f6
Laurent <laurent2002ruihong@yahou.com>
2023-11-17 23:01:58 +0100

    Inferface de login et creation de compte fonctionnelle

--- app/src/main/java/com/example/projet_android/CreateAccountActivity.java
+++ app/src/main/java/com/example/projet_android/CreateAccountActivity.java
@@ -1,30 +1,101 @@
 package com.example.projet_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
+    TextInputEditText aEmail,aPrenom,aNom,aPassword;
+    Button aBtnLogin, aBtnCreate;
+    FirebaseAuth mAuth;
+    ProgressBar aProgessBar;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent it = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(it);
+            finish();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_account);
 
-        final Button aBtnLogin = findViewById(R.id.button4);
+        mAuth=FirebaseAuth.getInstance();
+        aEmail=findViewById(R.id.email_create);
+        aPrenom=findViewById(R.id.Name);
+        aNom=findViewById(R.id.Surname);
+        aPassword=findViewById(R.id.password_create);
+        aBtnLogin = findViewById(R.id.button4);
+        aBtnCreate = findViewById(R.id.button3);
+        aProgessBar=findViewById(R.id.progessBar);
+
+
         aBtnLogin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent it = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(it);
             }
         });
-        final Button aBtnCreate = findViewById(R.id.button3);
         aBtnCreate.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-            }
+                aProgessBar.setVisibility(View.VISIBLE);
+                String email,password,nom,prenom;
+                email=aEmail.getText().toString();
+                password=aPassword.getText().toString();
+                nom=aNom.getText().toString();
+                prenom=aPrenom.getText().toString();
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(CreateAccountActivity.this,"Enter email",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    Toast.makeText(CreateAccountActivity.this,"Enter password",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(nom)){
+                    Toast.makeText(CreateAccountActivity.this,"Enter lastname",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(prenom)){
+                    Toast.makeText(CreateAccountActivity.this,"Enter name",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                aProgessBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(CreateAccountActivity.this, "Account Creation Successful.",
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(CreateAccountActivity.this, "Account Creation Failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                }
         });
 
     }
--- app/src/main/java/com/example/projet_android/LoginActivity.java
+++ app/src/main/java/com/example/projet_android/LoginActivity.java
@@ -1,36 +1,91 @@
 package com.example.projet_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextInputEditText aEmail,aPassword;
+    Button aBtnLogin, aBtnCreate;
+    FirebaseAuth mAuth;
+    ProgressBar aProgessBar;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent it = new Intent(getApplicationContext(), InterfacePrincipalActivity.class);
+            startActivity(it);
+            finish();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //test
         setContentView(R.layout.login_page);
 
         ImageView myImageView = (ImageView) findViewById(R.id.imageView);
         myImageView.setImageResource(R.drawable.logo_complet);
 
-        //bouton Login
-        final Button aBtnLogin = findViewById(R.id.button2);
+
+        aBtnLogin = findViewById(R.id.button2);
+        aBtnCreate = findViewById(R.id.button);
+        mAuth=FirebaseAuth.getInstance();
+        aEmail=findViewById(R.id.email);
+        aPassword=findViewById(R.id.password);
+        aProgessBar=findViewById(R.id.progessBar);
         aBtnLogin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent it = new Intent(getApplicationContext(), InterfacePrincipalActivity.class);
-                startActivity(it);
+                aProgessBar.setVisibility(View.VISIBLE);
+                String email,password,nom,prenom;
+                email=aEmail.getText().toString();
+                password=aPassword.getText().toString();
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(LoginActivity.this,"Enter email",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    Toast.makeText(LoginActivity.this,"Enter password",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                aProgessBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(LoginActivity.this, "Login Successful.", Toast.LENGTH_SHORT).show();
+                                    Intent it = new Intent(getApplicationContext(), InterfacePrincipalActivity.class);
+                                    startActivity(it);
+                                    finish();
+                                } else {
+                                    Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
             }
         });
 
-        // bouton Create
-        final Button aBtnCreate = findViewById(R.id.button);
         aBtnCreate.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent it = new Intent(getApplicationContext(), CreateAccountActivity.class);
