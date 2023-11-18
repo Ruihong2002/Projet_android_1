4a004975eef1687f882698af9cb616954c387db7
Laurent <laurent2002ruihong@yahou.com>
2023-11-18 17:01:54 +0100

    debut interface physique page mon profil

--- app/src/main/java/com/example/projet_android/PageMonProfilActivity.java
+++ app/src/main/java/com/example/projet_android/PageMonProfilActivity.java
@@ -5,20 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.google.firebase.auth.FirebaseUser;
 
 public class PageMonProfilActivity extends AppCompatActivity {
+    FirebaseUser aUser;
+    ImageButton aBtnBack,aBtnEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page_mon_profil);
-        final Button aBtnLogin = findViewById(R.id.button7);
-        aBtnLogin.setOnClickListener(new View.OnClickListener() {
+        aBtnBack = findViewById(R.id.Btnback);
+        aBtnEdit=findViewById(R.id.BtnEdit);
+        aBtnBack.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent it = new Intent(getApplicationContext(), InterfacePrincipalActivity.class);
                 startActivity(it);
             }
         });
+        aBtnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
